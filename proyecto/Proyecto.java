9c0b38dd4794c7d58f863c75f1bb671474a2bdf7
aquinoacordero <aquinoacordero@eniac33.danielcastelao.org>
2014-10-02 09:40:41 +0200

    cambios de 2º nivel

--- proyecto/Proyecto.java
+++ proyecto/Proyecto.java
@@ -16,6 +16,9 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*KKK 
+        pacho pringao
+        */
     }
     
 }
